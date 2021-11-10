abc609cbce89c1d9691e72e46996bb7a6447c93e
Nidsonr1 <nidsoncampos@hotmail.com>
2021-11-09 21:11:21 -0300

    Fixed Bugs

--- backend/ormconfig.ts
+++ backend/ormconfig.ts
@@ -6,7 +6,7 @@ module.exports = {
   "password": process.env.DB_PASSWORD,
   "database": process.env.DB_NAME, 
   "migrations": ["./src/database/migrations/*.ts"],
-  "entities": ["./src/modules/**/entities/*.ts"],
+  "entities": ["./src/**/entities/*.ts"],
   "cli": {
     "migrationsDir": "./src/database/migrations"
   }
